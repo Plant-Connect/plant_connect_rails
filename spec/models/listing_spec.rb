7786e658b26f4498641e5f37f1272b39ae5112ad
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-13 17:13:48 -0500

    Test: user_id and plant_id are present

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Listing, type: :model do
   describe 'validations' do
     it { should validate_presence_of :quantity }
     it { should validate_presence_of :category }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :plant_id }
 
     it { should validate_numericality_of :quantity }
   end
@@ -12,4 +14,6 @@ RSpec.describe Listing, type: :model do
     it { should belong_to :user }
     it { should belong_to :plant }
   end
+
+  
 end
