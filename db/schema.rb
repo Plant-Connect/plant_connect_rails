08c4ba0da07f33c40f9c38a6518695968ec06b13
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-12 20:55:33 -0500

    Add usernames to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_13_004243) do
+ActiveRecord::Schema.define(version: 2022_05_13_015453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_05_13_004243) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
