5743276abcc234589844caa479a2b149d9d1bbc0
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-13 17:09:05 -0500

    Refactor: update to valide presence of user_id

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,13 @@
 class Plant < ApplicationRecord
-  validates_presence_of :photo, :type, :description
+  # Model Validations
+  validates_presence_of :photo, 
+                        :type, 
+                        :description, 
+                        :user_id
+  
   validates :indoor, inclusion: { in: [true, false] }
   
+  # Model Relationships
   belongs_to :user
   has_many :listings
 end
