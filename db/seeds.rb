5da0032d302d7073b7e4a527160c9f6b33033705
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-12 20:48:35 -0500

    Add User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+@user1 = User.create(email: 'aedan@test.com', password: '123password', password_confirmation: '123password')
+@user2 = User.create(email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123')
+@user3 = User.create(email: 'emily@test.com', password: 'plantconnect', password_confirmation: 'plantconnect')
+@user4 = User.create(email: 'jerry@test.com', password: 'madprops', password_confirmation: 'madprops')
+@user5 = User.create(email: 'katie_a@test.com', password: 'seedly', password_confirmation: 'seedly')
+@user6 = User.create(email: 'katie_t@test.com', password: 'stemswap', password_confirmation: 'stemswap')
+@user7 = User.create(email: 'paul@test.com', password: 'plantydropper', password_confirmation: 'plantydropper')
+@user8 = User.create(email: 'steven@test.com', password: 'nogreenthumbs', password_confirmation: 'nogreenthumbs')
