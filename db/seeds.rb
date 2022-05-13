6825e5391cda86180179c3a78c3e851fddaf897e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-12 20:57:16 -0500

    Update seed data with usernames

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 
 User.destroy_all
 
-@user1 = User.create(email: 'aedan@test.com', password: '123password', password_confirmation: '123password')
-@user2 = User.create(email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123')
-@user3 = User.create(email: 'emily@test.com', password: 'plantconnect', password_confirmation: 'plantconnect')
-@user4 = User.create(email: 'jerry@test.com', password: 'madprops', password_confirmation: 'madprops')
-@user5 = User.create(email: 'katie_a@test.com', password: 'seedly', password_confirmation: 'seedly')
-@user6 = User.create(email: 'katie_t@test.com', password: 'stemswap', password_confirmation: 'stemswap')
-@user7 = User.create(email: 'paul@test.com', password: 'plantydropper', password_confirmation: 'plantydropper')
-@user8 = User.create(email: 'steven@test.com', password: 'nogreenthumbs', password_confirmation: 'nogreenthumbs')
+@user1 = User.create(username: 'Aedan', email: 'aedan@test.com', password: '123password', password_confirmation: '123password')
+@user2 = User.create(username: 'Brenda', email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123')
+@user3 = User.create(username: 'Emily', email: 'emily@test.com', password: 'plantconnect', password_confirmation: 'plantconnect')
+@user4 = User.create(username: 'Jerry', email: 'jerry@test.com', password: 'madprops', password_confirmation: 'madprops')
+@user5 = User.create(username: 'Katie', email: 'katie_a@test.com', password: 'seedly', password_confirmation: 'seedly')
+@user6 = User.create(username: 'Katie', email: 'katie_t@test.com', password: 'stemswap', password_confirmation: 'stemswap')
+@user7 = User.create(username: 'Paul', email: 'paul@test.com', password: 'plantydropper', password_confirmation: 'plantydropper')
+@user8 = User.create(username: 'Steven', email: 'steven@test.com', password: 'nogreenthumbs', password_confirmation: 'nogreenthumbs')
