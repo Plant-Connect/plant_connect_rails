79dd648a4008e81c620377c119baef009cad3518
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-12 20:29:43 -0500

    Test: user will not create if validations fail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,23 @@ RSpec.describe User, type: :model do
       expect(User.last.email).to eq(user.email)
     end
     
+    it 'does not create a User when email is already taken' do 
+      user1 = User.create(email: 'steven@test.com', password: 'password123', password_confirmation: 'password123')
+      user2 = User.create(email: 'steven@test.com', password: 'anotherpassword', password_confirmation: 'anotherpassword')
+      
+      expect(User.count).to eq(1)
+      expect(user1).to be_instance_of User
+      expect(User.last.email).to eq(user1.email)
+    end
     
+    it 'does not create a User when password and confirmation are not matching' do 
+      user1 = User.create(email: 'steven@test.com', password: 'password123', password_confirmation: 'password123')
+      user2 = User.create(email: 'anotheremail@test.com', password: 'anotherpassword', password_confirmation: 'thisisthewrongpassword')
+      
+      expect(User.count).to eq(1)
+      expect(user1).to be_instance_of User
+      expect(User.last).to eq(user1)
+      expect(User.last.email).to eq(user1.email)
+    end
   end
 end
