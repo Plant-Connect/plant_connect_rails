321b2834dbb69360d7fcd0535c57c7edf39bd70e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-16 16:14:26 -0500

    Refactor: remove API endpoint that is not needed for coordinates

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -5,14 +5,6 @@ class MapquestService
       faraday.params['key'] = ENV['mapquest_key']
     end
   end
-
-  def self.get_city_coordinates(city)
-    response = conn.get('geocoding/v1/address') do |request|
-      request.params['location'] = city
-      request.params['outFormat'] = 'json'
-    end
-    json = JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
-  end
   
   def self.directions(start, destination)
     response = conn.get('directions/v2/route') do |request|
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -2,27 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MapquestService, :vcr do
   context 'class methods' do 
-    context '#get_city_coordinates(city)' do
-      before(:each) do 
-        @location_data = MapquestService.get_city_coordinates('St. Louis, MO')
-      end
-
-      it 'returns a hash of location data' do 
-        expect(@location_data).to be_a Hash
-      end
-
-      it 'hash has expected data' do 
-        expect(@location_data.keys).to eq([:lat, :lng])
-      end
-
-      it 'latitude and longitude are returned as a Float' do 
-        expect(@location_data[:lat]).to be_a Float
-        expect(@location_data[:lat]).to eq(38.630276)
-        
-        expect(@location_data[:lng]).to be_a Float
-        expect(@location_data[:lng]).to eq(-90.200309)
-      end
-    end
 
     context '#directions(start, destination)' do
       before(:each) do 
