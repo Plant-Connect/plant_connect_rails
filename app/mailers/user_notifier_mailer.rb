ccaa6ebf89a65f9d244154f05956472f246966e6
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-15 22:54:10 -0500

    Mailer sends email with required data

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,8 +1,14 @@
 class UserNotifierMailer < ApplicationMailer
 
-  def send_listing_email(email, listing)
-    @email = email 
-    mail( :to => @email, 
+  def send_listing_email(recipient_id, listing_id)
+
+    @recipient = User.find_by_id(recipient_id)
+    @listing = Listing.find_by_id(listing_id)
+
+    @recipient_email = @recipient.email
+    @recipient_name = @recipient.username
+
+    mail( :to => @recipient_email, 
           :subject => 'New plant listings in your area!')
   end
 end
