75c7dd3e23645a55c2da198bfab9beaf8729827a
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-16 19:16:02 -0400

    Write out skeleton tests

---
+++ spec/requests/listings/create_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Listing POST/Create", type: :request do
+  describe "When a user sneds proper info in a form and attempts to post a new Listing," do
+    it "sends a successful response if form was filled out properly" do
+
+    end
+
+    it "sends back a json response with all necessary info" do
+
+    end
+
+    it "sends an unsuccesful response if form filled out improperly, or incomplete" do
+
+    end
+  end
+end
