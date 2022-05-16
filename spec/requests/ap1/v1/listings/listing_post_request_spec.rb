894b2a5bbff4d93fde462ce4910facdea4bb52c0
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-16 19:37:53 -0400

    write out first test.  No route error

--- spec/requests/ap1/v1/listings/listing_post_request_spec.rb
+++ spec/requests/ap1/v1/listings/listing_post_request_spec.rb
@@ -6,9 +6,16 @@ RSpec.describe "Listing POST/Create", type: :request do
       user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
       plant = Plant.create(photo: "photo string", plant_type: "plant type string", user_id: user.id)
       listing_params = {
+        quantity: 20,
+        category: :clippings,
+        user_id: user.id,
+        description: "this is a plant clipping please have it for free",
+        plant_id: plant.id
+                       }.to_json
 
-      }
-      post "/listings"
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post "/listings", headers: headers, params: listing_params
     end
 
     it "sends back a json response with all necessary info" do
