3fcc09e4b4d98633323c50517b677f77ba6a21e6
Aedan Yturralde <aedanyturralde@gmail.com>
2022-05-16 17:55:59 -0600

    test: define listing FactoryBot

--- spec/factories/plants.rb
+++ spec/factories/plants.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :plant do
     photo { Faker::LoremFlickr.image }
     plant_type { Faker::Cannabis.strain }
-    indoor { ["true", "false"].sample }
+    indoor { Faker::Boolean.boolean(true_ratio: 0.2) }
     user
   end
 end
