65680e61199d7cc75f03135df58829de74af67a8
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-16 19:30:45 -0400

    remove old spec file and add new one with proper file structure

--- spec/requests/ap1/v1/listings/listing_post_request_spec.rb
+++ spec/requests/ap1/v1/listings/listing_post_request_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe "Listing POST/Create", type: :request do
   describe "When a user sneds proper info in a form and attempts to post a new Listing," do
     it "sends a successful response if form was filled out properly" do
+      user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
+      plant = Plant.create(photo: "photo string", plant_type: "plant type string", user_id: user.id)
+      listing_params = {
 
+      }
+      post "/listings"
     end
 
     it "sends back a json response with all necessary info" do
