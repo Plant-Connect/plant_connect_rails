5fb6e77e04c0b0869c157c9d1301eed7780d31d1
Aedan Yturralde <aedanyturralde@gmail.com>
2022-05-16 17:37:47 -0600

    fix: user Factory username now correctly assigns username

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    username { Faker::Internet.first_name }
+    username { Faker::FunnyName.name }
     location { Faker::Address.city }
     password_digest {Faker::Alphanumeric.alphanumeric(number: 10)}
   end
