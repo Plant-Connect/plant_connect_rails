1d5b4d7332a1cdaa2fecebc2fd2515d7a6a52a8d
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-16 12:19:27 -0500

    Add active as boolean to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_162503) do
+ActiveRecord::Schema.define(version: 2022_05_16_171500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_162503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.boolean "active"
     t.index ["plant_id"], name: "index_listings_on_plant_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
