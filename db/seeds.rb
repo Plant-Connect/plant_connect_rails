1d69a8dd2a0fb29bf40a51af01f3ea95c0349da0
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-16 18:39:09 -0500

    Update seeds to test mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,14 @@
 User.destroy_all
 
 @user1 = User.create(username: 'Aedan', email: 'aedan@test.com', password: '123password', password_confirmation: '123password', location: 'Denver County, CO')
-@user2 = User.create(username: 'Brenda', email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123', location: 'Denve rCounty, CO')
+@user2 = User.create(username: 'Brenda', email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123', location: 'Denver County, CO')
 @user3 = User.create(username: 'Emily', email: 'emily@test.com', password: 'plantconnect', password_confirmation: 'plantconnect', location: 'Denver County, CO')
 @user4 = User.create(username: 'Jerry', email: 'jerry@test.com', password: 'madprops', password_confirmation: 'madprops', location: 'Denver County, CO')
 @user5 = User.create(username: 'Katie', email: 'katie_a@test.com', password: 'seedly', password_confirmation: 'seedly', location: 'Jefferson County, CO')
 @user6 = User.create(username: 'Katie', email: 'katie_t@test.com', password: 'stemswap', password_confirmation: 'stemswap', location: 'Jefferson County, CO')
 @user7 = User.create(username: 'Paul', email: 'paul@test.com', password: 'plantydropper', password_confirmation: 'plantydropper', location: 'Boulder County, CO')
 @user8 = User.create(username: 'Steven', email: 'steven@test.com', password: 'nogreenthumbs', password_confirmation: 'nogreenthumbs', location: 'Boulder County, CO')
+
+@plant = @user1.plants.create(photo: 'https://user-images.githubusercontent.com/91357724/168396277-da1c9486-fbe9-4e9f-8fb7-68ed88e42489.jpeg', plant_type: 'snake plant', indoor: true)
+      
+@listing = @user1.listings.create!(plant_id: @plant.id, quantity: 2, category: 1, description: 'This is the listings description', rooted: true, active: true)
