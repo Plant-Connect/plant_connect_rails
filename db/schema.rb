e8a1d353bd49c2cb0623144eec46e2f61d41afeb
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-16 11:26:17 -0500

    Add location to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_164323) do
+ActiveRecord::Schema.define(version: 2022_05_16_162503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_05_15_164323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "location"
   end
 
   add_foreign_key "listings", "plants"
