a986ca9ff472bc28d2bd235a7e751dc774812a79
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-16 18:42:45 -0500

    Test: method sorts Users by location and sends mailer to all users in the same location

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Listing, type: :model do
   describe 'validations' do
     it { should validate_presence_of :quantity }
     it { should validate_presence_of :category }
+    it { should validate_presence_of :description }
     it { should validate_presence_of :user_id }
     it { should validate_presence_of :plant_id }
-    it { should validate_presence_of :description }
-
 
     it { should validate_numericality_of :quantity }
   end
@@ -22,8 +21,8 @@ RSpec.describe Listing, type: :model do
       user = User.create(username: 'Steven', email: 'steven@test.com', password: 'password123', password_confirmation: 'password123', location: 'St. Louis, MO')
       plant = user.plants.create(photo: 'https://user-images.githubusercontent.com/91357724/168396277-da1c9486-fbe9-4e9f-8fb7-68ed88e42489.jpeg', plant_type: 'snake plant', indoor: true)
 
-      listing = user.listings.create!(plant_id: plant.id, quantity: 2, category: 1, description: 'This is the listings description', rooted: true)
-
+      listing = user.listings.create!(plant_id: plant.id, quantity: 2, category: 1, description: 'This is the listings description', rooted: true, active: true)
+      
       expect(Listing.last).to eq(listing)
       expect(listing.category).to eq('clippings')
     end
@@ -31,11 +30,29 @@ RSpec.describe Listing, type: :model do
     it 'allows category as a string as long as it matches one provided by enum' do 
       user = User.create(username: 'Steven', email: 'steven@test.com', password: 'password123', password_confirmation: 'password123', location: 'St. Louis, MO')
       plant = user.plants.create(photo: 'https://user-images.githubusercontent.com/91357724/168396277-da1c9486-fbe9-4e9f-8fb7-68ed88e42489.jpeg', plant_type: 'snake plant', indoor: true)
-  
-      listing = user.listings.create!(plant_id: plant.id, quantity: 2, category: 'seeds', description: 'This is the listings description', rooted: true)
+      
+      listing = user.listings.create!(plant_id: plant.id, quantity: 2, category: 'seeds', description: 'This is the listings description', rooted: true, active: true)
       
       expect(Listing.last).to eq(listing)
       expect(listing.category).to eq('seeds')
+    end
+  end
+  
+  describe 'instance methods' do 
+    describe 'send_new_listing_email' do 
+      it 'runs mailer method on all users in the same county as listing' do 
+        user1 = User.create(username: 'Steven', email: 'steven@test.com', password: 'password123', password_confirmation: 'password123', location: 'Denver County, CO')
+        plant = user1.plants.create(photo: 'https://user-images.githubusercontent.com/91357724/168396277-da1c9486-fbe9-4e9f-8fb7-68ed88e42489.jpeg', plant_type: 'snake plant', indoor: true)
+        listing = user1.listings.create!(plant_id: plant.id, quantity: 2, category: 1, description: 'This is the listings description', rooted: true, active: true)
+        user2 = User.create(username: 'Brenda', email: 'brenda@test.com', password: 'password123', password_confirmation: 'password123', location: 'Denver County, CO')
+        user3 = User.create(username: 'Emily', email: 'emily@test.com', password: 'plantconnect', password_confirmation: 'plantconnect', location: 'Denver County, CO')
+        user4 = User.create(username: 'Jerry', email: 'jerry@test.com', password: 'madprops', password_confirmation: 'madprops', location: 'Boulder County, CO')
+        user5 = User.create(username: 'Katie', email: 'katie_a@test.com', password: 'seedly', password_confirmation: 'seedly', location: 'Jefferson County, CO')     
+        
+        email_recipients = listing.send_new_listing_email
+        
+        expect(email_recipients).to eq([user2, user3])
+      end
     end
   end
 end
