00fa1a6896dde7ba6d2a0f55a42375f885ea75db
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-17 11:42:20 -0400

    Add create method

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::ListingsController < ApplicationController
 
-  # There will be some code we need to include on the Create whenever we are saving a new listing in order to send the email notifications to Users. The line of code is below. 
-  # This method will need the ID of the objects to be passed as the `perform` function does NOT pass through objects and will convert any argument into a serialize-able string. 
+  # There will be some code we need to include on the Create whenever we are saving a new listing in order to send the email notifications to Users. The line of code is below.
+  # This method will need the ID of the objects to be passed as the `perform` function does NOT pass through objects and will convert any argument into a serialize-able string.
   # ListingSenderJob.perform_async(@user2.id, @listing.id)
 
-  def index 
+  def index
     # return all active listings (excluding users own listings)
     listings = Listing.active_listings_self_excluded(params[:user_id])
     render json: ListingSerializer.listings(listings,params[:user_id])
+  end
+
+  def create
+
   end
 end
