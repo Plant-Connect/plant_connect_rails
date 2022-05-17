739d9608130770cd0ec270641d18904e87622607
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-17 16:04:15 -0400

    write first half of bad response test

--- spec/requests/api/v1/listing_patch_request_spec.rb
+++ spec/requests/api/v1/listing_patch_request_spec.rb
@@ -17,5 +17,21 @@ RSpec.describe "Listing Patch/Create", type: :request do
       patch "/api/v1/listings", headers: headers, params: patch_params
 
     end
+
+    xit "recieves a bad repsonse if the form is filled out incompletely or if the data types don't match" do
+      user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
+      plant = user.plants.create(photo: 'photo string', plant_type: 'plant_type', indoor: true)
+      listing = user.listings.create(quantity: 10, category: "plant", description: "blah blah", plant_id: plant.id)
+
+      patch_params = {
+        quantity: nil,
+        listing_id: listing.id
+      }.to_json
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      patch "/api/v1/listings", headers: headers, params: patch_params
+
+    end
   end
 end
