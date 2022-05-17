bd537ee6e58233da97d1393f67db246b7af84470
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-17 16:43:24 -0400

    Start sad path test

--- spec/requests/api/v1/listing_patch_request_spec.rb
+++ spec/requests/api/v1/listing_patch_request_spec.rb
@@ -16,9 +16,28 @@ RSpec.describe "Listing Patch/Create", type: :request do
 
       patch "/api/v1/listings", headers: headers, params: patch_params
 
+      expect(response.status).to eq(200)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to be_a Hash
+      expect(json[:data]).to be_a Hash
+      expect(json[:data][:type]).to eq("listing")
+      expect(json[:data][:attributes]).to be_a Hash
+      expect(json[:data][:attributes][:listing_id]).to be_a Integer
+      expect(json[:data][:attributes][:active]).to eq(true)
+      expect(json[:data][:attributes][:quantity]).to be_a Integer
+      expect(json[:data][:attributes][:quantity]).to eq(20)
+      expect(json[:data][:attributes][:category]).to be_a String
+      expect(json[:data][:attributes][:category]).to eq("plant")
+      expect(json[:data][:attributes][:rooted]).to eq(true)
+      expect(json[:data][:attributes][:plant_id]).to eq(plant.id)
+      expect(json[:data][:attributes][:user_id]).to eq(user.id)
+
+      expect(json[:data][:attributes][:quantity]).to_not eq(10)
     end
 
-    xit "recieves a bad repsonse if the form is filled out incompletely or if the data types don't match" do
+    it "recieves a bad repsonse if the form is filled out incompletely or if the data types don't match" do
       user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
       plant = user.plants.create(photo: 'photo string', plant_type: 'plant_type', indoor: true)
       listing = user.listings.create(quantity: 10, category: "plant", description: "blah blah", plant_id: plant.id)
@@ -32,6 +51,10 @@ RSpec.describe "Listing Patch/Create", type: :request do
       headers = { 'CONTENT_TYPE' => 'application/json' }
 
       patch "/api/v1/listings", headers: headers, params: patch_params
+
+      expect(response.status).to eq(400)
+
+      json = JSON.parse(response.body, symbolize_names: true)
 
     end
   end
