732b630badc403992292ef9bace1ce965c19b035
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-17 14:19:57 -0500

    Configure VCR in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,3 +77,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<openweather_key>') { ENV['openweather_key'] }
+  config.default_cassette_options = { re_record_interval: 30.days }
+  config.configure_rspec_metadata!
+end
