753662b034df253600876575788895e2cfb83839
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-17 13:09:47 -0400

    Add sad path test

--- spec/requests/api/v1/listing_post_request_spec.rb
+++ spec/requests/api/v1/listing_post_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "Listing POST/Create", type: :request do
   describe "When a user sends proper info in a form and attempts to post a new Listing," do
     it "sends a successful response if form was filled out properly, and has all proper info in a json object" do
-
       user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
       plant = user.plants.create(photo: 'photo string', plant_type: 'plant_type', indoor: true)
       listing_params = {
@@ -39,7 +38,25 @@ RSpec.describe "Listing POST/Create", type: :request do
     end
 
     it "sends an unsuccesful response if form filled out improperly, or incomplete" do
+      user  = User.create(username: 'Aedan2', email: 'aedan2@test.com', password: '123password', password_confirmation: '123password', location: 'Denver, CO')
+      plant = user.plants.create(photo: 'photo string', plant_type: 'plant_type', indoor: true)
+      listing_params = {
+        quantity: 20,
+        category: :clippings,
+        user_id: user.id,
+        plant_id: plant.id
+                       }.to_json
 
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post "/api/v1/listings", headers: headers, params: listing_params
+
+      expect(response.status).to eq(400)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to be_a Hash
+      expect(json[:data][:attributes][:description]).to eq("Form not filled out all the way, Please try again")
     end
   end
 end
