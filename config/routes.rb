0d407768f11fdc884498a21fe72747560d02f02c
Paul Leonard <paulpaulleonard@gmail.com>
2022-05-17 16:33:00 -0400

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :listings, only: [:index, :create, :update]
+      resources :listings, only: [:index, :create]
+      patch '/listings', to: 'listings#update'
     end
   end
 end
