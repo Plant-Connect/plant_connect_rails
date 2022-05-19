6af4c9790c9972c132c18a5ceb566449018facaa
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-19 13:03:54 -0500

    Update sidekiq configure settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,18 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
+
+ Sidekiq.configure_server do |config|
+
+   Rails.logger = Sidekiq::Logging.logger
+
+   config.redis = { :url => MyApp.redis[:url], :namespace => 'sidekiq', :size => 5 }
+ end
+
+ Sidekiq.configure_client do |config|
+   config.redis = { :url => MyApp.redis[:url], :namespace => 'sidekiq', :size => 1 }
+ end
