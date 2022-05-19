628297ea53790c57c0206ce848d25b615baa2044
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-19 13:05:55 -0500

    Revert changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,2 @@
-# uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
-# REDIS = Redis.new(:url => uri)
-
- module MyApp
-   class << self
-     def redis
-       @redis ||= Redis.new(url: (ENV['REDISTOGO_URL'] || 'redis://127.0.0.1:6379'))
-     end
-   end
- end
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+REDIS = Redis.new(:url => uri)
