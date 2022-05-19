13f0a1b0812500303aa4225d3e4ddba8fe6b0301
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-19 10:31:15 -0500

    Configure sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://:p747d795ec49db166d901f210bbcbfcb6247f0ab7971da3d28fde9a77ff13fd77@ec2-34-203-181-194.compute-1.amazonaws.com:30899' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://:p747d795ec49db166d901f210bbcbfcb6247f0ab7971da3d28fde9a77ff13fd77@ec2-34-203-181-194.compute-1.amazonaws.com:30899' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 end
+
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://:p747d795ec49db166d901f210bbcbfcb6247f0ab7971da3d28fde9a77ff13fd77@ec2-34-203-181-194.compute-1.amazonaws.com:30899' }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://:p747d795ec49db166d901f210bbcbfcb6247f0ab7971da3d28fde9a77ff13fd77@ec2-34-203-181-194.compute-1.amazonaws.com:30899' }
+# end
