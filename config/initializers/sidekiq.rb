f4dfdda8fc10bb869085c4f10b24f6c7431de5a8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-19 13:36:37 -0500

    Final attempt to get sidekiq working

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
+# end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+end
+
+Sidekiq::Extensions.enable_delay!
