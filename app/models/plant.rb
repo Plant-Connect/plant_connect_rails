d7be2d6cd15eb3d5339cd2a2d1d63a13c27bd46a
Aedan Yturralde <aedanyturralde@gmail.com>
2022-05-20 12:04:17 -0600

    add commented out instance method get_image_url for future use

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,9 @@
 class Plant < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   # Model Validations
-  validates_presence_of :photo, 
-                        :plant_type, 
+  validates_presence_of :photo,
+                        :plant_type,
                         :user_id
   
   validates :indoor, inclusion: { in: [true, false] }
@@ -9,6 +11,13 @@ class Plant < ApplicationRecord
   # Model Relationships
   belongs_to :user
   has_many :listings, dependent: :destroy
-  # not a DB attribute like :photo is 
+  # not a DB attribute like :photo
   has_one_attached :image
+
+  # url_for is a cloudinary method (I think), we may want to implement it in our serializer once we
+  # get to the point where we're uploading to cloudinary
+
+  # def get_image_url
+  #   url_for(self.image)
+  # end
 end
