e33873a290bc6fe6d8f94d77784055d796215f89
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-21 15:26:19 -0500

    Remove model and spec files for dropped table

--- app/models/conversation_message.rb
+++
@@ -1,4 +0,0 @@
-class ConversationMessage < ApplicationRecord
-  belongs_to :conversation
-  belongs_to :message
-end
--- spec/models/conversation_message_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ConversationMessage, type: :model do
-
-  describe 'relationships' do
-    it { should belong_to :conversation }
-    it { should belong_to :message }
-  end
-end 
