1863b3972aee0e986fbc5510df6938ed5275651f
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-21 15:01:51 -0500

    Add ActionCable for conversations connection

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => "/cable"
+
   namespace :api do
     namespace :v1 do
       resources :listings, only: [:index, :create]
