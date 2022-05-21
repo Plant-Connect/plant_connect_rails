c481500cc4b5e9b035cd69dc86fd8b06f883bde1
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-21 14:40:20 -0500

    Add Conversation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_21_193226) do
+ActiveRecord::Schema.define(version: 2022_05_21_193931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "listings", force: :cascade do |t|
     t.integer "quantity"
