5c5c25ca052468323b89a3d66eb82da4154eeae9
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-24 15:13:04 -0500

    it returns an error if the conversation_id does not exist

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -4,17 +4,17 @@ class Api::V1::ConversationsController < ApplicationController
     if params[:user_id].blank?
       json_response({ data: { message: 'user_id is required to view conversations' } }, :bad_request)
     else
-      user = User.find_by_id(params[:user_id])
+      user = User.find(params[:user_id])
       conversations = user.conversations
       json_response(ConversationSerializer.new(conversations))
     end 
   end
   
   def show
-    if params[:user_id].blank? || params[:id].blank?
-      json_response({ data: { message: 'user_id and conversation_id are required' } }, :bad_request)
+    conversation = Conversation.find(params[:id])
+    if params[:user_id].blank?
+      json_response({ data: { message: 'user_id is required to view conversations' } }, :bad_request)
     else
-      conversation = Conversation.find(params[:id])
       json_response(ConversationSerializer.new(conversation))
     end
   end
