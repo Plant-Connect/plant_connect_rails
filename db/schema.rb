96dcd7bfe11fe818da31d67c5e933093a6c60958
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-25 17:19:43 -0500

    Add plant_name to conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_213924) do
+ActiveRecord::Schema.define(version: 2022_05_25_221426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_05_25_213924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "listing_id"
+    t.string "name"
     t.index ["listing_id"], name: "index_conversations_on_listing_id"
   end
 
